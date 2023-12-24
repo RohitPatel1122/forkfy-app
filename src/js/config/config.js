8a479a2d3f7c6811f282cd6849fe2a644a8e0749
Rohit Patel <rohitpatel.1122@gmail.com>
2023-12-24 13:34:44 +0530

    Add dummy keys

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const API_TIMEOUT_IN_SEC = 10;
 export const RESULT_PER_PAGE = 10;
-export const API_KEY = "";
+export const API_KEY = "267c0173-f719-4772-a38a-dbb9a2aaee86";
 export const MODEL_CLOSE_TIMEOUT_IN_SEC = 2.5;
